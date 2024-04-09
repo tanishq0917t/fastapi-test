1e33bcf4cbeb967d88d1d7e925f26dd07582cf6b
Tanishq Rawat <67191912+tanishq0917t@users.noreply.github.com>
2024-04-09 18:34:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,8 @@ from bson import ObjectId
 import json
 
 app=FastAPI()
-data=None
-def loadData():
-    global data
-    with open('config.json','r')as file:
-        data=json.load(file)
-
-loadData()
-count=100
 ds={}
-
-def details():
+def details(collectionName):
     username = data['user']
     password = data['pass']
     clusterId= data['clusterId']
@@ -25,11 +16,35 @@ def details():
     uri=f"mongodb+srv://{encoded_username}:{encoded_password}@{clusterId}.mongodb.net/"
     client = MongoClient(uri, server_api=ServerApi('1'))
     db = client['student']
-    collection = db['studentInfo']
+    collection = db[collectionName]
     return collection
 
+data=None
+count=99
+def loadData():
+    global data
+    global count
+    with open('config.json','r')as file:
+        data=json.load(file)
+    collection=details("rollInfo")
+    cursor = collection.find({})
+    mx=99
+    for doc in cursor:
+        ds[doc['roll']]=doc['obj']
+        if mx<doc['roll']:mx=doc['roll']
+    count=mx+1
+
+
+loadData()
+
+
+
+
+
+
+
 def addStudent(student):
-    collection=details()
+    collection=details("studentInfo")
     result = collection.insert_one(student)
     return str(result.inserted_id)
 
@@ -39,12 +54,14 @@ def create_student(student: dict):
     result = addStudent(student)
     ds[count]=result
     roll_no=count
+    rollCollection=details("rollInfo")
+    rollCollection.insert_one({'roll':roll_no,'obj':result})
     count=count+1
     return {'id' : roll_no}
 
 @app.get("/students",status_code=200)
 def get_data(rollNo: int = 0,country: str="",age: int=-1):
-    collection=details()
+    collection=details("studentInfo")
     if rollNo==0:
         if len(country)==0 and age==-1:
             cursor = collection.find({})
@@ -72,19 +89,26 @@ def get_data(rollNo: int = 0,country: str="",age: int=-1):
 
 @app.delete("/students", status_code = 200)
 def delete_data():
-    collection=details()
+    global count
+    collection=details("studentInfo")
     result = collection.delete_many({})
+
+    collection=details("rollInfo")
+    collection.delete_many({})
+    count = 100
     return {"message": f"Deleted {result.deleted_count} documents"}
 
 @app.delete("/students/{student_id}", status_code = 200)
 def delete_data(student_id : int):
-    collection=details()
+    collection=details("studentInfo")
     roll_no = ds[student_id]
     result = collection.delete_one({"_id":ObjectId(roll_no)})
+    collection=details("rollInfo")
+    collection.delete_one({"roll":student_id})
     return {"message": f"Deleted {result.deleted_count} documents"}
 
 @app.patch("/students/{student_id}",status_code=204)
 def update_data( student_id : int ,updated_data : dict):
-    collection=details()
+    collection=details("studentInfo")
     obj_id=ds[student_id]
     collection.update_one({"_id": ObjectId(obj_id)}, {"$set": updated_data['updated_data']})
